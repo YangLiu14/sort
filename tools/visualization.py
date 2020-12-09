95dd5dd7e47310d069b85aaba5ae0a30cdbed3e0
YangLiu14 <ga27kuh@mytum.de>
2020-12-09 10:33:47 +0100

    add viz script

--- tools/visualization.py
+++ tools/visualization.py
@@ -1,7 +1,9 @@
-import sys
-import os
+import argparse
 import colorsys
 import glob
+import json
+import os
+import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,7 +15,7 @@ from multiprocessing import Pool
 from functools import partial
 from subprocess import call
 
-
+BASE_DIR = "/home/kloping/OpenSet_MOT/"
 class SegmentedObject:
     def __init__(self, bbox, class_id, track_id):
         self.bbox = bbox
@@ -116,6 +118,80 @@ def process_sequence(seq_fpaths, tracks_folder, img_folder, output_folder, max_f
         visualize_sequences(seq_id, tracks, max_frames_seq, img_folder, annot_frames, output_folder, draw_boxes, create_video)
 
 
+def process_sequence_coco(track_result_map, img_id2name, datasrc, img_folder, output_folder, max_frames, annot_frames_dict,
+                     draw_boxes=True, create_video=True):
+    """
+    Args:
+        track_result_map: Dict { img_id: List[detections] }. tracking result
+        img_id2name: a Dict, that maps img_id to img_name (datasrc/video_name/frame_name)
+        img_folder:
+        output_folder:
+        max_frames:
+        annot_frames_dict:
+        draw_boxes:
+        create_video:
+
+    Returns:
+
+    """
+
+    # print("Processing sequence", seq_name)
+    os.makedirs(output_folder, exist_ok=True)
+
+    split = img_folder.split('/')
+    if "train" in split:
+        gt_path = os.path.join(BASE_DIR, "data/TAO/annotations/train.json")
+    elif "val" in split:
+        gt_path = os.path.join(BASE_DIR, "data/TAO/annotations/validation.json")
+    else:
+        raise Exception("invalid img_folder")
+
+    with open(gt_path, 'r') as f:
+        gt_dict = json.load(f)
+    gt_images = gt_dict["images"]
+    img_name2id = dict()
+    for img in gt_images:
+        img_name = img["file_name"]
+        if img["id"] not in img_id2name.keys():
+            img_name2id[img_name] = -1
+        img_name2id[img_name] = img["id"]
+
+    # tracks: List of
+    #   { video_name: {frame_id: List[SegmentedObject]}, sorted in the order of frame }
+    tracks = dict()
+    for video, frames in annot_frames_dict.items():
+        frames.sort()
+        if video not in tracks.keys():
+            tracks[video] = dict()
+        for idx, frame in enumerate(frames):
+            frame_id = idx + 1
+            tracks[video][frame_id] = list()
+            frame_name = '/'.join(frame.split('/')[7:])
+            img_id = img_name2id[frame_name]
+            if img_id not in track_result_map.keys():
+                # There are not detections in the current frame
+                dets = list()
+            else:
+                dets = track_result_map[img_id]
+            # Convert each dets to SegmentedObject
+            dets.sort(key=lambda k: k['score'])
+            for det in dets:
+                tracks[video][frame_id].append(SegmentedObject(bbox=det["bbox"],
+                                                                   class_id=det["category_id"],
+                                                                   track_id=det["track_id"]))
+            # only keep detections with top 10 highest scores.
+            tracks[video][frame_id] = tracks[video][frame_id][:10]
+
+    # Now we have tracks
+    # Get the seq_id, which is just the video name
+    # Get the max_frames
+
+    for seq_id in annot_frames_dict.keys():
+        annot_frames = annot_frames_dict[seq_id]
+        max_frames_seq = len(annot_frames)
+        visualize_sequences(seq_id, tracks, max_frames_seq, img_folder, annot_frames, output_folder, draw_boxes, create_video)
+
+
 def visualize_sequences(seq_id, tracks, max_frames_seq, img_folder, annot_frames, output_folder, draw_boxes=True, create_video=True):
     colors = generate_colors()
     dpi = 100.0
@@ -124,8 +200,9 @@ def visualize_sequences(seq_id, tracks, max_frames_seq, img_folder, annot_frames
     frames_with_annotations = [frame.split('/')[-1] for frame in annot_frames]
     # img_sizes = next(iter(tracks[seq_id])).bbox
     for t in range(max_frames_seq):
-        print("Processing frame", frames_with_annotations[t])
-        filename_t = img_folder + "/" + seq_id + "/" + frames_with_annotations[t]
+        print("Processing frame", annot_frames[t])
+        # filename_t = img_folder + "/" + seq_id + "/" + frames_with_annotations[t]
+        filename_t = annot_frames[t]
         img = np.array(Image.open(filename_t), dtype="float32") / 255
         img_sizes = img.shape
         fig = plt.figure()
@@ -178,46 +255,122 @@ def main():
     # output_folder = sys.argv[3]
     # seqmap_filename = sys.argv[4]
 
-    data_srcs = ["ArgoVerse", "BDD", "Charades", "LaSOT", "YFCC100M"]
-    curr_data_src = data_srcs[4]
-    tracks_folder = "/home/kloping/git-repos/sort/output_minhit0/" + curr_data_src
-    img_folder = "/home/kloping/OpenSet_MOT/data/TAO/TAO_VAL/val/" + curr_data_src
-    output_folder = "/home/kloping/OpenSet_MOT/Tracking/track_results/viz/SORT_minhit0/" + curr_data_src
-    seqmap_filenames = sorted(glob.glob(tracks_folder + '/*' + '.txt'))
+    parser = argparse.ArgumentParser(description='Visualization script for tracking result')
+    parser.add_argument("--track_format", help="The file format of the tracking result", type=str, default='mot')
+    args = parser.parse_args()
 
-    # seqmap, max_frames = load_seqmap(seqmap_filename)
+    if args.track_format == "mot":
+        """
+        The tracking result is stored in the mot-format.
+            https://motchallenge.net/instructions/
+        """
+        data_srcs = ["ArgoVerse", "BDD", "Charades", "LaSOT", "YFCC100M"]
+        curr_data_src = data_srcs[4]
+        tracks_folder = "/home/kloping/git-repos/sort/output_minhit0/" + curr_data_src
+        img_folder = "/home/kloping/OpenSet_MOT/data/TAO/frames/val/" + curr_data_src
+        output_folder = "/home/kloping/OpenSet_MOT/Tracking/track_results/viz/SORT_minhit0/" + curr_data_src
+        seqmap_filenames = sorted(glob.glob(tracks_folder + '/*' + '.txt'))
 
-    max_frames = dict()
-    annot_frames = dict()  # annotated frames for each sequence
-    for seq_fpath in seqmap_filenames:
-        seq_name = seq_fpath.split('/')[-1][:-4]
+        # seqmap, max_frames = load_seqmap(seqmap_filename)
 
-        # Get the max number of frames of the current sequence
-        num_frames = 0
-        with open(seq_fpath, 'r') as f:
-            for line in f:
-                line = line.strip()
-                fields = line.split(",")
-                num_frames = int(fields[0])
-        max_frames[seq_name] = num_frames
+        max_frames = dict()
 
-    # Get the annotated frames in the current sequence.
-    txt_fname = "../data/tao/val_annotated_{}.txt".format(curr_data_src)
-    with open(txt_fname) as f:
-        content = f.readlines()
-    content = ['/'.join(c.split('/')[1:]) for c in content]
-    annot_seq_paths = [os.path.join(img_folder, x.strip()) for x in content]
+        for seq_fpath in seqmap_filenames:
+            seq_name = seq_fpath.split('/')[-1][:-4]
 
-    for s in annot_seq_paths:
-        seq_name = s.split('/')[-2]
-        if seq_name not in annot_frames.keys():
-            annot_frames[seq_name] = []
-        annot_frames[seq_name].append(s)
+            # Get the max number of frames of the current sequence
+            num_frames = 0
+            with open(seq_fpath, 'r') as f:
+                for line in f:
+                    line = line.strip()
+                    fields = line.split(",")
+                    num_frames = int(fields[0])
+            max_frames[seq_name] = num_frames
 
-    process_sequence_part = partial(process_sequence, max_frames=max_frames, annot_frames_dict=annot_frames,
-                                    tracks_folder=tracks_folder, img_folder=img_folder, output_folder=output_folder)
+        annot_frames = dict()  # annotated frames for each sequence
+        # Get the annotated frames in the current sequence.
+        txt_fname = "../data/tao/val_annotated_{}.txt".format(curr_data_src)
+        with open(txt_fname) as f:
+            content = f.readlines()
+        content = ['/'.join(c.split('/')[1:]) for c in content]
+        annot_seq_paths = [os.path.join(img_folder, x.strip()) for x in content]
 
-    process_sequence_part(seqmap_filenames)
+        for s in annot_seq_paths:
+            seq_name = s.split('/')[-2]
+            if seq_name not in annot_frames.keys():
+                annot_frames[seq_name] = []
+            annot_frames[seq_name].append(s)
+
+        process_sequence_part = partial(process_sequence, max_frames=max_frames, annot_frames_dict=annot_frames,
+                                        tracks_folder=tracks_folder, img_folder=img_folder, output_folder=output_folder)
+        process_sequence_part(seqmap_filenames)
+
+    elif args.track_format == "coco":
+        """
+        The tracking result is stored in the coco-format.
+            https://github.com/TAO-Dataset/tao/blob/master/docs/evaluation.md
+        """
+        max_frames = dict()  # dummy variable here that is not used.
+        datasrc = "Charades"  # ["ArgoVerse", "BDD", "Charades", "LaSOT", "YFCC100M"]
+
+        tracks_folder = "/home/kloping/OpenSet_MOT/Tracking/tao_track_results/SORT_val/sort_tao_val_results.json"
+        img_folder = "/home/kloping/OpenSet_MOT/data/TAO/frames/val/"
+        output_folder = "/home/kloping/OpenSet_MOT/Tracking/tao_track_results/viz/SORT_val/" + datasrc
+
+
+        annot_frames = dict()  # annotated frames for each sequence
+        # Get the annotated frames in the current sequence.
+        txt_fname = "../data/tao/val_annotated_{}.txt".format(datasrc)
+        with open(txt_fname) as f:
+            content = f.readlines()
+        content = ['/'.join(c.split('/')[1:]) for c in content]
+        annot_seq_paths = [os.path.join(img_folder, datasrc, x.strip()) for x in content]
+
+        for s in annot_seq_paths:
+            seq_name = s.split('/')[-2]
+            if seq_name not in annot_frames.keys():
+                annot_frames[seq_name] = []
+            annot_frames[seq_name].append(s)
+            annot_frames[seq_name].sort()
+
+        # Load tracking result json file
+        with open(tracks_folder, 'r') as f:
+            track_results = json.load(f)
+
+        # Get the (train/val) GT annotation json from img_folder
+        #   expected img_folder: /home/kloping/OpenSet_MOT/data/TAO/frames/val/
+        split = img_folder.split('/')
+        if "train" in split:
+            gt_path = os.path.join(BASE_DIR, "data/TAO/annotations/train.json")
+        elif "val" in split:
+            gt_path = os.path.join(BASE_DIR, "data/TAO/annotations/validation.json")
+        else:
+            raise Exception("invalid img_folder")
+
+        with open(gt_path, 'r') as f:
+            gt_dict = json.load(f)
+        gt_images = gt_dict["images"]
+        img_id2name = dict()
+        for img in gt_images:
+            img_name = img["file_name"][4:-4]
+            if img["id"] not in img_id2name.keys():
+                img_id2name[img["id"]] = ''
+            img_id2name[img["id"]] = img_name
+
+        # Filter, let the images in the current datasrc remain.
+        track_result_map = dict()  # {img_id: List[detections]}
+        for det in track_results:
+            img_name = img_id2name[det["image_id"]]
+            video_name = '/'.join(img_name.split('/')[:2])  # datasrc/video_name
+            curr_d = img_name.split('/')[0]
+            if curr_d == datasrc:
+                if det["image_id"] not in track_result_map.keys():
+                    track_result_map[det["image_id"]] = list()
+                track_result_map[det["image_id"]].append(det)
+
+        process_sequence_part = partial(process_sequence_coco, max_frames=max_frames, datasrc=datasrc, annot_frames_dict=annot_frames,
+                                        img_id2name=img_id2name, img_folder=img_folder, output_folder=output_folder)
+        process_sequence_part(track_result_map)
 
     # with Pool(10) as pool:
     #     pool.map(process_sequence_part, seqmap)
